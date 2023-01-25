0432008dabe7341b3f08e8ac0a0d3770e28d5bac
M.Martinez <wsenjuw@gmail.com>
2023-01-25 17:00:38 -0300

    commit antes del upgrade

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
   const [menu, setMenu] = useState(false);
   const [modal, setModal] = useState(false);
 
+  console.log(new Date);
 
 
   return (
@@ -20,8 +21,12 @@ export default function App() {
         onPress={() => menu === true ? setMenu(false) : setModal(false)}
         style={{ ...styles.darkBackground, width: (menu === true || modal === true ? "100%" : 0) }} />
       {modal === true &&
-        <View
-          style={{ width: '90%', height: "70%", backgroundColor: '#f1f1f1', position: 'absolute' }}>
+        <View style={styles.modal}>
+          <Text style={{
+            alignSelf: 'center'
+            , fontSize: 20, maxWidth: 200, textAlign: 'center'
+          }} >Fecha actual</Text>
+
 
         </View>
       }
@@ -35,7 +40,7 @@ export default function App() {
           <Text style={styles.menuItemText}>Modo</Text>
         </TouchableOpacity>
       </ScrollView>
-    </View>
+    </View >
   );
 }
 
@@ -61,24 +66,32 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 0,
     paddingTop: 20,
-    display:'flex',
-    flexDirection:'column',
-    alignContent:'center',
-    
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
   },
-  menuItem:{
-    width:"92%",
-    height:60,
-    backgroundColor:"#D9D9D9",
-    display:'flex',
-    alignContent:'center',
-    justifyContent:'center',
-    marginLeft:'4%',
+  menuItem: {
+    width: "92%",
+    height: 60,
+    backgroundColor: "#D9D9D9",
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+    marginLeft: '4%',
     margin: 10
   },
-  menuItemText:{
-    fontSize:18,
-    maxWidth:200,
-    marginLeft:10
+  menuItemText: {
+    fontSize: 18,
+    maxWidth: 200,
+    marginLeft: 10
+  },
+  modal: {
+    width: '90%',
+    height: "70%",
+    paddingVertical:50,
+    backgroundColor: '#f1f1f1',
+    position: 'absolute',
+    display: 'flex',
+    alignContent: 'center',
   }
 });
